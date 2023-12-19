b0afb917717d225e6dda47b37e4e1ad0584d1b08
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-19 10:16:15 +0900

    Update perfume.js

--- perfume.js
+++ perfume.js
@@ -1,1 +1,4 @@
 function imgchange(){document.getElementById('perfume').src='';}
+function imgchange(){document.getElementById('perfume').src='';}
+function imgchange(){document.getElementById('perfume').src='';}
+function imgchange(){document.getElementById('perfume').src='CLEAN-クラシック-アルティメイト-オードパルファム.jpg';}
