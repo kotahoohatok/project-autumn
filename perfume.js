a0d6df12aed1ad4f088a02b6fd8e664b7dcf7912
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-19 10:18:43 +0900

    Update perfume.js

--- perfume.js
+++ perfume.js
@@ -1,4 +1,4 @@
 function imgchange(){document.getElementById('perfume').src='replica-lazy-sunday-morning.png';}
 function imgchange(){document.getElementById('perfume').src='Hermes-ナイルの庭.jpg';}
-function imgchange(){document.getElementById('perfume').src='';}
+function imgchange(){document.getElementById('perfume').src='Diptyque-オードトワレ-フィロシコス.jpg';}
 function imgchange(){document.getElementById('perfume').src='CLEAN-クラシック-アルティメイト-オードパルファム.jpg';}
