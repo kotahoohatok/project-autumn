f07a8fc5f741412714c8cf55c156cbec626d2763
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-21 14:32:02 +0900

    Update perfume.js

--- perfume.js
+++ perfume.js
@@ -1,4 +1,4 @@
-function imgchange1(){document.getElementById('perfume').src='image/replica-lazy-sunday-morning.png';}
-function imgchange2(){document.getElementById('perfume').src='image/Hermes-ナイルの庭.jpg';}
-function imgchange3(){document.getElementById('perfume').src='image/Diptyque-オードトワレ-フィロシコス.jpg';}
-function imgchange4(){document.getElementById('perfume').src='image/CLEAN-クラシック-アルティメイト-オードパルファム.jpg';}
+function imgchange1(){document.getElementById('perfume').src='./image/replica-lazy-sunday-morning.png';}
+function imgchange2(){document.getElementById('perfume').src='./image/Hermes-ナイルの庭.jpg';}
+function imgchange3(){document.getElementById('perfume').src='./image/Diptyque-オードトワレ-フィロシコス.jpg';}
+function imgchange4(){document.getElementById('perfume').src='./image/CLEAN-クラシック-アルティメイト-オードパルファム.jpg';}
