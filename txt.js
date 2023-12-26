c2c7d7494ac779d164c8ed23898eb70f4dc2283d
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-26 21:05:20 +0900

    Delete txt.js

--- txt.js
+++
@@ -1,4 +0,0 @@
-function txtchange1(){document.getElementById('txt').innerHTML='replica-lazy-sunday-morning';}
-function txtchange2(){document.getElementById('txt').innerHTML='Hermes-ナイルの庭';}
-function txtchange3(){document.getElementById('txt').innerHTML='Diptyque-オードトワレ-フィロシコス';}
-function txtchange4(){document.getElementById('txt').innerHTML='CLEAN-クラシック-アルティメイト-オードパルファム';}
