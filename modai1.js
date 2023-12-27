b20e71a46b5d45ed1e285f3e8f8efb0e0dad8880
kotahoohatok <131092051+kotahoohatok@users.noreply.github.com>
2023-12-27 12:14:06 +0900

    Update modai1.js

--- modai1.js
+++ modai1.js
@@ -1,8 +1,7 @@
 function mondai1(){
-  if(document.getElementById('answer1').value != '3'){
-    alert('はずれ');
-  }
-  else{
-    alert('あたり！');
-  }
+if(document.getElementById('answer1'). value < '3')
+{alert('あたり！');}
+else{alert('はずれ');
 }
+}
+
